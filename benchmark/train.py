f5eee28cb1242d208c61447c57102a98e3481c31
yuantailing <yuantailing@126.com>
2017-10-20 04:14:38 +0800

    add some classification nets configs

--- benchmark/train.py
+++ benchmark/train.py
@@ -9,6 +9,7 @@ import os
 import settings
 import six
 import subprocess
+import sys
 
 
 env = {
@@ -22,7 +23,37 @@ cfgs = [
         'train_dir': 'products/train_logs_alexnet_v2',
         'train_image_size': '224',
         'batch_size': '64',
-    }
+    },
+    {
+        'model_name': 'inception_v4',
+        'train_dir': 'products/train_logs_inception_v4',
+        'train_image_size': '235',
+        'batch_size': '64',
+    },
+    {
+        'model_name': 'overfeat',
+        'train_dir': 'products/train_logs_overfeat',
+        'train_image_size': '231',
+        'batch_size': '64',
+    },
+    {
+        'model_name': 'resnet_v2_50',
+        'train_dir': 'products/train_logs_resnet_v2_50',
+        'train_image_size': '224',
+        'batch_size': '64',
+    },
+    {
+        'model_name': 'resnet_v2_152',
+        'train_dir': 'products/train_logs_resnet_v2_152',
+        'train_image_size': '224',
+        'batch_size': '64',
+    },
+    {
+        'model_name': 'vgg_16',
+        'train_dir': 'products/train_logs_vgg_16',
+        'train_image_size': '224',
+        'batch_size': '64',
+    },
 ]
 
 cfg_common = {
@@ -49,6 +80,8 @@ def main(model_name):
         args.append('--{}={}'.format(k, v))
 
     new_env = os.environ.copy()
+    if 'CUDA_VISIBLE_DEVICES' in new_env:
+        env['CUDA_VISIBLE_DEVICES'] = new_env['CUDA_VISIBLE_DEVICES']
     new_env.update(env)
 
     for k, v in env.items():
@@ -59,4 +92,4 @@ def main(model_name):
 
 
 if __name__ == '__main__':
-    main('alexnet_v2')
+    main(sys.argv[1])
