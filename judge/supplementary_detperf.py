cd71d08814cd176e73a965830a105cf21bd63092
yuantailing <yuantailing@126.com>
2017-11-19 16:04:33 +0800

    add supplementary_detperf

--- judge/supplementary_detperf.py
+++ judge/supplementary_detperf.py
@@ -6,13 +6,34 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import json
+import random
 import settings
+import six
 
 
 def main():
+    assert six.PY3
+    random.seed(1)
+
     with open(settings.DETECTION_REPORT) as f:
         report = json.load(f)
     performance = report['performance']
+
+    with open(settings.STAT_FREQUENCY) as f:
+        frequency = json.load(f)
+    freq_order = [o['text'] for o in frequency]
+    with open('../classification/products/cates.json') as f:
+        cates = json.load(f)
+    cates = [(c['text'], [], c['cate_id']) for c in sorted(random.sample(cates[10:1000], 50), key=lambda o: -o['trainval'])]
+    cates.sort(key=lambda t: freq_order.index(t[0]))
+    for text, a, cate_id in cates:
+        s = r'\begin{minipage}{3.5mm} \includegraphics[width=\linewidth]{figure/texts/' + '1_{}.png'.format(cate_id) + r'} \end{minipage}'
+        for szname, _ in settings.SIZE_RANGES:
+            APn = performance[szname]['texts'].get(text)
+            s += ' & ' + ('{:.1f}'.format(APn['AP'] * 100) if APn is not None else '-')
+        s += ' & {}'.format(performance['all']['texts'].get(text, {'n': 0})['n'])
+        s += r' & {} \\'.format(list(filter(lambda o: o['text'] == text, frequency))[0]['trainval'])
+        print(s)
 
     def check(k, attr_id):
         if attr_id < len(settings.ATTRIBUTES):
